e100cd2cb59f1a07ec334dc0f48eb656d3ba7e75
olivergskelton-star <olivergskelton@googlemail.com>
2026-08-21 13:18:06 +0100

    Refresh cache for randomised answer positions

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='pass-gcse-v4';
+const CACHE='pass-gcse-v5';
 const ASSETS=['./','./index.html','./questions-core.js','./questions-macbeth.js','./questions-christmas-carol.js','./questions-inspector-calls.js','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
